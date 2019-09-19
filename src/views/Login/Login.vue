b8c97fb9d3fd077e33b144fb1b43561991d6019e
lojze <lojze@foxmail.com>
2019-09-19 20:10:44 +0800

    up

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <LoginHeader></LoginHeader>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import LoginHeader from "./LoginHeader.vue";
+@Component({
+  components: {}
+})
+export default class Login extends Vue {}
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/views/Login/Password.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="password">Password</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class Password extends Vue {}
+</script>
+
+<style lang="sass">
+
+</style>
